b86d6aabb72749fee9120c6d3af7a7ac9da3a3f2
T3chnogod <39940654+ShoaibPathan@users.noreply.github.com>
2019-05-20 16:50:12 +0530

    Bug fixes
    Bug fixes

--- MusicApp/MusicApp/WebService/APIChartResponse.swift
+++ MusicApp/MusicApp/WebService/APIChartResponse.swift
@@ -1,9 +1,9 @@
 //
 //  APIChartResponse.swift
-//  Last.Fm Search
+//  MusicApp
 //
-//  Created by Owner on 06/03/2019.
-//  Copyright © 2019 Thomas. All rights reserved.
+//  Created by shohib mohammed pathan on 20/05/19.
+//  Copyright © 2019 AppKrafting. All rights reserved.
 //
 
 import Foundation
